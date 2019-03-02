323df8ff14ae8ac1a17a622aa6e6b7efb7e83e32
Alex Wasinger <25324676+awasinger@users.noreply.github.com>
2019-03-01 21:12:43 -0600

    Welcome page kind of

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,9 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <nav class="navbar">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="">test</a>
-            </div>
-        </nav>
-    </body>
-</html>
+<div class="title-page">
+	<h1 class="title">Saluta</h1>
+</div>
+
+@endsection
