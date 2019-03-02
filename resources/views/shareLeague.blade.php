77cabc418fa9d085955153dacebd3a6a55f40854
Joseph Gioia <jegioia37@gmail.com>
2019-03-01 21:13:06 -0600

    basic views

---
+++ resources/views/shareLeague.blade.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <title>Share Your League</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <div class="card">
+      <h1>Share Your League: </h1>
+      {{$leagueCode}}
+      <h3>Send this code to your friends, so they can join the league</h3>
+    </div>
+  </body>
+</html>
