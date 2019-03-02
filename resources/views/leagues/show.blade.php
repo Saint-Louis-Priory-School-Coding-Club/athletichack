229e5ee0b8a6984267759b2cc91aab8e7794dce1
Alex Wasinger <25324676+awasinger@users.noreply.github.com>
2019-03-02 00:54:20 -0600

    file structure and some stuff

---
+++ resources/views/leagues/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+    <table class="table is-bordered is-striped is-fullwidth">
+      <thead>
+        <th>Name</th>
+        <th>Last Week Weight Loss (%)</th>
+        <th>Last Week Exercise Count (Min)</th>
+      </thead>
+      <tbody>
+          <tr>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+          </tr>
+          <tr>
+              <td>test</td>
+              <td>test</td>
+              <td>test</td>
+          </tr>
+      </tbody>
+    </table>
+@endsection
