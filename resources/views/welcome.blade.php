00b4ea78c820717a71a16199281069f6bd685661
Joseph Gioia <jegioia37@gmail.com>
2019-03-02 12:00:00 -0600

    motto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 	<h1 id="header" class="title">Saluta</h1>
 </div>
 <div class="title-page no-select">
-	<h2 id="caption" class="caption">Challenge not only yourself, but your friends!</h2>
+	<h2 id="caption" class="caption">Challenge yourself and your friends!</h2>
 </div>
 
 
